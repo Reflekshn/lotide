40c0560d1c59f15868948f51c9a6aabfa6de67fd
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-11-27 22:03:03 +0000

    Added a header comment to the file

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+// Assert function that takes in two values to compare and console.log's an appropriate message to the console
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
