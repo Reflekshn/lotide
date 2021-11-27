41146f1512483e59871f2c32cfbb8af208350641
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-11-27 06:52:32 +0000

    Function that will report back how many instances of each string in ItemsToCount were found in the allItems array of strings

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+// Assert function that takes in two values to compare and console.log's an appropriate message to the console
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Function that will report back how many instances of each string in itemsToCount were found in the allItems array of strings.
+const countOnly = function (allItems, itemsToCount) {
+  // Empty object to hold the results. The name and the amount of instances found in the array.
+  let results = {};
+
+  // Loop over all items in the array
+  for (const item of allItems) {
+    // Only count items that have been requested to be counted
+    if (itemsToCount[item]) {
+      // increment the counter for each item:
+      //   set a property with that string key to:
+      //     the value that was already there (or zero if nothing there) with 1 added to it.
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
